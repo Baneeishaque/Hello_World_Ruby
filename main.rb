4e248894cda3ac8dc55ff573fc407f6e41c73c33
Banee Ishaque K <baneeishaque@gmail.com>
2019-09-02 23:44:44 +0530

    In

---
+++ main.rb
@@ -0,0 +1,21 @@
+if __FILE__ == $0
+  mg = MegaGreeter.new
+  mg.say_hi
+  mg.say_bye
+
+  # Change name to be "Zeke"
+  mg.names = "Zeke"
+  mg.say_hi
+  mg.say_bye
+
+  # Change the name to an array of names
+  mg.names = ["Albert", "Brenda", "Charles",
+              "Dave", "Engelbert"]
+  mg.say_hi
+  mg.say_bye
+
+  # Change to nil
+  mg.names = nil
+  mg.say_hi
+  mg.say_bye
+end
